2b7190d2e91fada426230671627c99ecb38abf9a
GustavoSilvaC <gustavodasilvacardoso02@gmail.com>
2024-04-25 17:23:00 -0300

    commit inicial

--- src/Model/Usuario.java
+++ src/Model/Usuario.java
@@ -1,5 +1,47 @@
 package Model;
 
-public class Usuario {
+public class Usuario extends Pessoa{
+    private String login;
+    private String senha;
+    private String nomeSocial;
+
+    public Usuario(int id, String nome, String fone1, String fone2, String email, String cpfCnpj,
+            String rgInscricaoEstadual, String cep, String cidade, String bairro, String logradouro,
+            String complemento, String login, String senha, String nomeSocial) {
+        super(id, nome, fone1, fone2, email, cpfCnpj, rgInscricaoEstadual, cep, cidade, bairro, logradouro,
+             complemento);
+        this.login = login;
+        this.senha = senha;
+        this.nomeSocial = nomeSocial;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeSocial() {
+        return nomeSocial;
+    }
+
+    public void setNomeSocial(String nomeSocial) {
+        this.nomeSocial = nomeSocial;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\nNome Social: " + this.nomeSocial;
+    }
 
 }
